5ba4ec11b829e836079bd4047791b0459f2efdbe
BatoBazarov <102617433+BatoBazarov@users.noreply.github.com>
2022-05-19 21:32:49 +0300

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -7,20 +7,22 @@ public class Employee {
     public double salary;
     public int age;
 
+    public Employee(String name, String position, String email, String phone, double salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
     public int getAge() {
         return age;
     }
-    public Employee (String name, String position, String email, String phone, double salary, int age){
-            this.name = name;
-            this.position = position;
-            this.email = email;
-            this.phone = phone;
-            this.salary = salary;
-            this.age = age;
-        }
-        public () {
-            System.out.println("Name: " + name + " | " + "Position: " + position + " | " + "Email: " + email + " | " +
-                    "Phone: " + phone + " | " + "Salary: " + salary + " | " + "Age: " + age);
-        }
+
+    public String toString() {
+        return  ( "Name: " + name+ "\n-" + " Position: " + position + " Email: " + email + " Phone: " + phone + " Salary " + salary + " Age "
+                + age);
     }
 }
+
